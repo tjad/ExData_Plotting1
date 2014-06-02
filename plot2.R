94981e2a7149bbd50f49d049190f35767b6ead26
Tjad Clark <tjataz@gmail.com>
2014-06-02 20:37:09 +0200

    added plot2.R

---
+++ plot2.R
@@ -0,0 +1,12 @@
+setAs("character","myDate", function(from) as.Date(from, format="%d/%m/%Y") )
+
+df <- read.csv2('household_power_consumption.txt', na.strings = '?', colClasses=c('Date'='myDate'))
+
+fromDate <- as.Date('2007-02-01')
+toDate <- as.Date('2007-02-02')
+
+df <- df[df$Date == fromDate | df$Date == toDate,]
+
+plot(as.numeric(Global_active_power)~ Date, df, type="l", col='black', ylab='Global Active Power(kilowatts)', xact='n')
+
+dev.copy(png,'plot2.png')
